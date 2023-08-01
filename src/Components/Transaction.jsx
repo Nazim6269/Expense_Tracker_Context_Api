1eba028ae4bb7dc7096e7f22c5c5b7abfc25a5e3
nazimuddin <nazimuddin102001@gmail.com>
2023-08-01 11:35:51 +0600

    first commit

---
+++ src/Components/Transaction.jsx
@@ -0,0 +1,24 @@
+import { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+
+const Transaction = (props) => {
+  const { text, amount, id } = props.transaction;
+  const sign = amount < 0 ? "-" : "+";
+  const { deleteTransaction } = useContext(GlobalContext);
+
+  return (
+    <div>
+      <li className={amount < 0 ? "minus" : "plus"}>
+        {text}{" "}
+        <span>
+          {sign}${Math.abs(amount)}
+        </span>
+        <button onClick={() => deleteTransaction(id)} className="delete-btn">
+          x
+        </button>
+      </li>
+    </div>
+  );
+};
+
+export default Transaction;
